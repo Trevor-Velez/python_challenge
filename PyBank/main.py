e2c4b1d8deff5587c649395ea990a585447d192e
Trevor Velez <trevorvelez1993@gmail.com>
2021-07-02 00:29:07 -0700

    Working with correct change list

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[41]:
+
+
 import os
 import csv
 
+
+# In[42]:
+
+
 datafile = 'Resources/budget_data.csv'
+
+
+# In[43]:
+
 
 with open(datafile) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -9,56 +23,73 @@ with open(datafile) as csvfile:
     
     count = 0
     sumofPL = 0
-    maxList = []
+    monthList = []
     changes = 0
     previousPL = 0
+    changeList = []
     
     for row in csvreader:
-        count += 1
-        sumofPL += int(row[1])
-        maxList.append(int(row[1]))
-
-        changes += (int(row[1]) - previousPL)
+        count = count + 1
+        sumofPL = sumofPL + int(row[1])
+        change = int(row[1]) - previousPL
+        changeList.append(change)
+        monthList.append(row[0])
         previousPL = int(row[1])
 
 
-        
-    maxProfit = max(maxList)
-    maxLoss = min(maxList)
-    maxProfitMonth = ""
-    maxLossMonth = ""
-    averageChange = changes / count
-    
-with open(datafile) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader, None)   
-    
-    for row in csvreader:
-        if int(row[1]) == maxProfit:
-            maxProfitMonth = row[0]
-        elif int(row[1]) == maxLoss:
-            maxLossMonth = row[0]
-            
+# In[44]:
+
+
+maxChange = max(changeList)
+maxChangeIndex = changeList.index(maxChange)
+maxChangeMonth = monthList[maxChangeIndex]
+
+minChange = min(changeList)
+minChangeIndex = changeList.index(minChange)
+minChangeMonth = monthList[minChangeIndex]
+
+
+# In[45]:
+
+
+changeList.pop(0)
+averageChange = sum(changeList) / len(changeList)
+
+
+# In[46]:
+
 
 print("Financial Analysis")
 print("----------------------------")
 print(f'Total Months: {count}')
 print(f'Total: {sumofPL}')
 print(f'Average Change: ${round(averageChange, 2)}')
-print(f'Greatest Increase in Profits: {maxProfitMonth} (${maxProfit})')
-print(f'Greatest Decrease in Profits: {maxLossMonth} (${maxLoss})')
+print(f'Greatest Increase in Profits: {maxChangeMonth} (${maxChange})')
+print(f'Greatest Decrease in Profits: {minChangeMonth} (${minChange})')
+
+
+# In[47]:
+
 
 output_path = 'Analysis/output.txt'
 
-with open(output_path, 'w') as textfile:
 
-    # Initialize csv.writer
-    #csvwriter = csv.writer(csvfile)
+# In[48]:
+
+
+with open(output_path, 'w') as textfile:
 
     textfile.write("Financial Analysis\n")
     textfile.write("----------------------------\n")
     textfile.write(f'Total Months: {count}\n')
     textfile.write(f'Total: {sumofPL}\n')
     textfile.write(f'Average Change: ${round(averageChange, 2)}\n')
-    textfile.write(f'Greatest Increase in Profits: {maxProfitMonth} (${maxProfit})\n')
-    textfile.write(f'Greatest Decrease in Profits: {maxLossMonth} (${maxLoss})\n')
+    textfile.write(f'Greatest Increase in Profits: {maxChangeMonth} (${maxChange})\n')
+    textfile.write(f'Greatest Decrease in Profits: {minChangeMonth} (${minChange})\n')
+
+
+# In[ ]:
+
+
+
+
